ac52f8a396454a1dd3459b4b34e221ffb4604520
anu <163709469+Yogxx@users.noreply.github.com>
2025-11-11 22:57:53 +0700

    Add files via upload

---
+++ luci-app-openclash/root/etc/openclash/custom/openclash_custom_firewall_rules.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+. /usr/share/openclash/log.sh
+. /lib/functions.sh
+
+LOG_OUT "Tip: Start Add Custom Firewall Rules..."
+
+exit 0
---
+++ luci-app-openclash/root/etc/openclash/custom/openclash_custom_overwrite.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+. /usr/share/openclash/ruby.sh
+. /usr/share/openclash/log.sh
+. /lib/functions.sh
+
+LOG_OUT "Tip: Start Running Custom Overwrite Scripts..."
+LOGTIME=$(echo $(date "+%Y-%m-%d %H:%M:%S"))
+LOG_FILE="/tmp/openclash.log"
+#Config Path
+CONFIG_FILE="$1"
+
+exit 0
