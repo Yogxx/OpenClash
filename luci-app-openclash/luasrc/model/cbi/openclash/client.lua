d0bedb5876c31aef7e8cdb3f8b8a9e1ab0612c03
anu <163709469+Yogxx@users.noreply.github.com>
2026-01-09 01:30:34 +0700

    Update client.lua

--- luci-app-openclash/luasrc/model/cbi/openclash/client.lua
+++ luci-app-openclash/luasrc/model/cbi/openclash/client.lua
@@ -7,8 +7,7 @@ local UTIL = require "luci.util"
 local fs = require "luci.openclash"
 local uci = require("luci.model.uci").cursor()
 
-m = SimpleForm("openclash",translate("OpenClash"))
-m.description = translate("A Clash Client For OpenWrt")
+m = SimpleForm("openclash")
 m.reset = false
 m.submit = false
 
